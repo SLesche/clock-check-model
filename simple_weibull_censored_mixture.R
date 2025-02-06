30a6734ffd908296b23dcd6f95be3ce1856223e0
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2025-02-06 21:41:59 +0100

    simulate mixture functions

--- simple_weibull_censored_mixture.R
+++ simple_weibull_censored_mixture.R
@@ -1,8 +1,68 @@
 library(dplyr)
 library(rstan)
-library(model.matrix)
 library(survival)
 library(ggsurvfit)
+
+
+simulate_mixture_weibull <- function(n, g, k1, lambda1, lambda2){
+  ifelse(runif(n, 0, 1) <= g, rweibull(n, 1, lambda2), rweibull(n, k1, lambda1))
+}
+
+plot_pred_vs_actual <- function(pred_surv, actual_surv, event_status, data) {
+  # Create survival objects
+  pred_fit <- survfit(Surv(pred_surv, event_status) ~ 1)
+  actual_fit <- survfit(Surv(actual_surv, event_status) ~ 1, data = data)
+  
+  # Convert to data frames for ggplot
+  pred_df <- broom::tidy(pred_fit) %>% mutate(Type = "Predicted")
+  actual_df <- broom::tidy(actual_fit) %>% mutate(Type = "Actual")
+  
+  # Combine the data
+  survival_df <- bind_rows(pred_df, actual_df)
+  
+  # Plot both curves
+  ggplot(survival_df, aes(x = time, y = estimate, color = Type)) +
+    geom_step() +
+    labs(
+      x = "Days",
+      y = "Overall survival probability",
+      title = "Predicted vs. Actual Survival Curves"
+    ) +
+    theme_minimal()
+}
+
+simulated_data <- data.frame(
+  times = simulate_mixture_weibull(10000, 0.1, 2.45, 0.5, 0.2),
+  event = 1
+)
+
+stan_data <- list(
+  N = nrow(simulated_data),
+  times = simulated_data$times,
+  event = simulated_data$event,
+  K_k = 1,
+  K_lambda = 1,
+  K_lambda2 = 1,
+  K_mixture = 1,
+  X_k = model.matrix( ~1, data = simulated_data),
+  X_lambda = model.matrix(~ 1, data = simulated_data),
+  X_lambda2 = model.matrix(~ 1, data = simulated_data),
+  X_mixture = model.matrix(~ 1, data = simulated_data)
+)
+
+# Fit the model
+options(mc.cores = parallel::detectCores())
+fit <- stan(
+  file = "simple_weibull_censored_mixed_timedep.stan",
+  data = stan_data,
+  iter = 2000,  # Number of iterations
+  chains = 4,   # Number of MCMC chains
+  warmup = 500, # Number of warmup iterations
+  # control = list(adapt_delta = 0.95, max_treedepth = 15, stepsize = 0.01)
+)
+
+traceplot(fit)
+print(fit)
 
 data <- read.csv("weibull_data.csv")
 
@@ -72,8 +132,26 @@ fit <- stan(
 traceplot(fit)
 print(fit)
 
+# Example usage (assuming clean_data exists)
+plot_pred_vs_actual(
+  pred_surv = get_mixture_prediction(nrow(clean_data), 0.2, 2.45, 0.65, 1), 
+  actual_surv = clean_data$r_check,
+  event_status = clean_data$event,
+  data = clean_data
+)
+
+
+survfit2(Surv(get_mixture_prediction(1000, 0.1, 2.45, 0.65, 1)) ~ 1) %>% 
+  ggsurvfit() +
+  labs(
+    x = "Days",
+    y = "Overall survival probability"
+  )
+
+
+
 # Kaplan-Meier Plot
-survfit2(Surv(r_check, event) ~ is_first_guess, data = clean_data %>%  filter(r_check < 1.1)) %>% 
+survfit2(Surv(r_check, event) ~ 1, data = clean_data) %>% 
   ggsurvfit() +
   labs(
     x = "Days",
